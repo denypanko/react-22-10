e9accb1637faf0f292de5bd9c5b807c41b4f6856
Denys <denyspanasenko95@gmail.com>
2023-02-04 13:08:30 +0200

    add simple content

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// const title = React.createElement('h1', { id: 'title' }, 'Hello React.js')
+
+const title = <h1 id="title">Hello React</h1>
+
+const content = (
+    <div>
+        {title}
+        {10 + 10}
+        <ul>
+            <li>List item 1</li>
+            <li>List item 2</li>
+            <li>List item 3</li>
+        </ul>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam in
+            est provident fugit ad nihil quo assumenda! Fuga quod debitis minus
+            in officia aliquam aspernatur accusamus quibusdam. Magnam, totam ab.
+        </p>
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{content}</React.StrictMode>)
