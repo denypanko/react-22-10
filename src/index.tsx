60a51a5a30c0ebe28688002b55b98fc0cef1b1ec
Denys <denyspanasenko95@gmail.com>
2023-02-04 21:14:22 +0200

    add default props

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { List } from './List'
 
 type TitleProps = {
-    name: string
+    name?: string
     text: string
     age: number
 }
 
-const Title = ({ name, text, age }: TitleProps) => {
+const Title = ({ name = 'Jack', text, age }: TitleProps) => {
     return (
         <>
             <h1>Hello {name}</h1>
@@ -22,7 +22,6 @@ const App = () => {
     return (
         <div className="app">
             <Title
-                name="App"
                 text="Lorem ipsum dolor sit, amet consectetur adipisicing elit."
                 age={25}
             />
