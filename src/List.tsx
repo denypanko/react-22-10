bc151192a82fe2c261563608516017cad9e6ca96
Denys <denyspanasenko95@gmail.com>
2023-02-05 13:14:53 +0200

    add destructurated css modules

--- src/List.tsx
+++ src/List.tsx
@@ -1,8 +1,9 @@
-import './List.css'
+import classes from './List.module.css'
 
 type Props = {}
 export const List = (props: Props) => {
-    let data = false
+    let data = true
+    const { content, test, articleDesc } = classes
     return (
         <>
             <h1>List</h1>
@@ -11,7 +12,7 @@ export const List = (props: Props) => {
                 <li>2</li>
                 <li>3</li>
             </ul>
-            <p className={`${data ? 'content' : 'test'}`}>
+            <p className={`${articleDesc} ${data ? content : test}`}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
                 tenetur vel facilis ratione quisquam. Eos recusandae natus culpa
                 dolorum nostrum quasi quidem harum blanditiis eveniet sint in
