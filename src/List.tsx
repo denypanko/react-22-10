4f63f3c7bda9c70fad73b1447b11625ead67245e
Denys <denyspanasenko95@gmail.com>
2023-02-05 12:43:06 +0200

    add styleObj

--- src/List.tsx
+++ src/List.tsx
@@ -1,5 +1,6 @@
 type Props = {}
 export const List = (props: Props) => {
+    let data = false
     return (
         <>
             <h1>List</h1>
@@ -8,6 +9,18 @@ export const List = (props: Props) => {
                 <li>2</li>
                 <li>3</li>
             </ul>
+            <p
+                style={{
+                    color: 'white',
+                    backgroundColor: data ? 'purple' : 'tomato',
+                    padding: '15px',
+                }}
+            >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+                tenetur vel facilis ratione quisquam. Eos recusandae natus culpa
+                dolorum nostrum quasi quidem harum blanditiis eveniet sint in
+                molestias, pariatur exercitationem.
+            </p>
         </>
     )
 }
