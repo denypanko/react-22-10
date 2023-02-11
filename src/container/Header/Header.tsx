d604a042b2ff523d97229cb88ef93080bbd1b564
Denys <denyspanasenko95@gmail.com>
2023-02-11 15:31:06 +0200

    add CartHeader

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -5,6 +5,7 @@ import MenuIcon from '@mui/icons-material/Menu'
 import { Container } from '@mui/material'
 import Logo from 'components/Logo/Logo'
 import Menu from 'components/Menu/Menu'
+import CartHeader from 'components/CartHeader/CartHeader'
 import './Header.scss'
 
 type Props = {}
@@ -25,6 +26,7 @@ const Header = (props: Props) => {
                         </IconButton>
                         <Logo />
                         <Menu />
+                        <CartHeader />
                     </Toolbar>
                 </Container>
             </AppBar>
