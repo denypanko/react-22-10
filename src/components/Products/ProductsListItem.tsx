f4f1c8c2cdc3ec15962b0c0c60eb4d77022bc454
Denys <denyspanasenko95@gmail.com>
2023-02-19 22:28:00 +0200

    add bind to this

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -25,8 +25,15 @@ class ProductsListItem extends Component<Product, State> {
     constructor(props: Product) {
         super(props)
         this.state = {
-            count: 2,
+            count: 1,
         }
+        this.onIncrementClick = this.onIncrementClick.bind(this)
+    }
+
+    onIncrementClick() {
+        this.setState({
+            count: 2,
+        })
     }
 
     render() {
@@ -50,7 +57,12 @@ class ProductsListItem extends Component<Product, State> {
                             size="small"
                             value={this.state.count}
                         ></TextField>
-                        <Button variant="outlined">+</Button>
+                        <Button
+                            variant="outlined"
+                            onClick={this.onIncrementClick}
+                        >
+                            +
+                        </Button>
                     </div>
                 </CardContent>
                 <CardActions className="btns-wrap">
